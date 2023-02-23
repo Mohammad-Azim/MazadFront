ed25499552c28d2c0a3c067bc379aa92a511290f
Mohammad-Azim <mohammad.azim@optimumpartners.co>
2023-02-23 16:06:20 +0200

    Edit: the hub connection

--- src/app/components/product-bid-list/product-bid-list.component.ts
+++ src/app/components/product-bid-list/product-bid-list.component.ts
@@ -1,18 +1,20 @@
-import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { BidService } from 'src/app/services/bid/bid.service';
-import { Subscription } from 'rxjs';
 import * as signalR from '@microsoft/signalr';
+import { GlobalComponent } from 'src/app/global.component';
 
 @Component({
   selector: 'app-product-bid-list',
   templateUrl: './product-bid-list.component.html',
   styleUrls: ['./product-bid-list.component.css'],
 })
-export class ProductBidListComponent {
-  //implements OnInit, OnDestroy
+export class ProductBidListComponent implements OnInit {
   bids: any[] = [];
   @Input() productId!: string;
-  private bidSubscription!: Subscription;
+
+  connection = new signalR.HubConnectionBuilder()
+    .withUrl(GlobalComponent.ApiUrl + 'bid-hub')
+    .build();
 
   constructor(private bidService: BidService) {}
 
@@ -22,18 +24,16 @@ export class ProductBidListComponent {
         this.bids = response.data;
       }
     );
-  }
 
-  connection = new signalR.HubConnectionBuilder()
-    .withUrl('https://localhost:7287/api/bid-hub')
-    .build();
-
-  ngOnChanges() {
     this.connection.on('BidAdded', (bid) => {
       if (bid.productId == this.productId) {
         this.bids.push(bid);
       }
     });
     this.connection.start().catch((err) => document.write(err));
+  }
+
+  ngOnDestroy(): void {
+    this.connection.stop();
   }
 }
